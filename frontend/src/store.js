d15511841e12a5a049c0565925f114e2297cc826
Nadeem9121 <135756141+Nadeem9121@users.noreply.github.com>
2024-08-02 12:47:30 +0500

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ const store = configureStore({
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
     auth: authSliceReducer,
-  },
+  }
   // middleware: (getDefaultMiddleware) =>
   //   getDefaultMiddleware().concat(apiSlice.middleware),
   // devTools: true,
