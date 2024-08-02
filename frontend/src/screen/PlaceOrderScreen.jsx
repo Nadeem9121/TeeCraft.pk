cf4cfee07b445d02af6f81ce729a2acdf81f3a16
Nadeem9121 <135756141+Nadeem9121@users.noreply.github.com>
2024-08-02 12:44:08 +0500

    Update PlaceOrderScreen.jsx

--- frontend/src/screen/PlaceOrderScreen.jsx
+++ frontend/src/screen/PlaceOrderScreen.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Button, Col, Row, ListGroup, Image, Card } from "react-bootstrap";
 import { toast } from "react-toastify";
